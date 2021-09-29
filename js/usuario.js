b584ef59ccd047fde22dfadcaeb318208b4a90e1
samuel llanos <psllanos@misena.edu.co>
2021-09-29 12:39:20 -0500

    crear el procedimiento almacenado de listar usuarios, para luego mostrarlo en la vista de usuario también usaremos el plugin datatable para tener un mejor diseño.

--- js/usuario.js
+++ js/usuario.js
@@ -1,122 +1,121 @@
-function VerificarUsuario() {
+function VerificarUsuario(){
   var usu = $("#txt_usu").val();
   var con = $("#txt_con").val();
 
-  if (usu.length == 0 || con.length == 0) {
-    return Swal.fire(
-      "Mensaje De Advertencia",
-      "Llene los campos vacios",
-      "warning"
-    );
+  if(usu.length==0 || con.length==0){
+      return Swal.fire("Mensaje De Advertencia","Llene los campos vacios","warning");
   }
   $.ajax({
-    url: "../controlador/usuario/controlador_verificar_usuario.php",
-    type: "POST",
-    data: {
-      user: usu,
-      pass: con,
-    },
-  }).done(function (resp) {
-    if (resp == 0) {
-      Swal.fire(
-        "Mensaje De Error",
-        "Usuario y/o contrase\u00f1a incorrecta",
-        "error"
-      );
-    } else {
-      var data = JSON.parse(resp);
-      if (data[0][5] === "INACTIVO") {
-        return Swal.fire(
-          "Mensaje De Advertencia",
-          "Lo sentimos el usuario " +
-            usu +
-            " se encuentra suspendido, comuniquese con el administrador",
-          "warning"
-        );
+      url:'../controlador/usuario/controlador_verificar_usuario.php',
+      type:'POST',
+      data:{
+          user:usu,
+          pass:con
       }
-      $.ajax({
-        url: "../controlador/usuario/controlador_crear_session.php",
-        type: "POST",
-        data: {
-          idusuario: data[0][0],
-          user: data[0][1],
-          rol: data[0][6],
-        },
-      }).done(function (resp) {
-        Swal.fire({
-          title: "Bienvenido A SCSS",
-          html: "Usted esta siendo redireccionado espere <b></b> milisegundos.",
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: () => {
-            Swal.showLoading();
-            const b = Swal.getHtmlContainer().querySelector("b");
-            timerInterval = setInterval(() => {
-              b.textContent = Swal.getTimerLeft();
-            }, 100);
-          },
-          willClose: () => {
-            clearInterval(timerInterval);
-          },
-        }).then((result) => {
-          /* Read more about handling dismissals below */
-          if (result.dismiss === Swal.DismissReason.timer) {
-            location.reload();
+  }).done(function(resp){
+      if(resp==0){
+          Swal.fire("Mensaje De Error",'Usuario y/o contrase\u00f1a incorrecta',"error");
+      }else{
+          var data= JSON.parse(resp);
+          if(data[0][5]==='INACTIVO'){
+              return Swal.fire("Mensaje De Advertencia","Lo sentimos el usuario "+usu+" se encuentra suspendido, comuniquese con el administrador","warning");
           }
-        });
-      });
-    }
-  });
+          $.ajax({
+              url:'../controlador/usuario/controlador_crear_session.php',
+              type:'POST',
+              data:{
+                  idusuario:data[0][0],
+                  user:data[0][1],
+                  rol:data[0][6]
+              }
+          }).done(function(resp){
+              let timerInterval
+              Swal.fire({
+              title: 'BIENVENIDO AL SISTEMA',
+              html: 'Usted sera redireccionado en <b></b> milisegundos.',
+              timer: 2000,
+              timerProgressBar: true,
+              onBeforeOpen: () => {
+                  Swal.showLoading()
+                  timerInterval = setInterval(() => {
+                  const content = Swal.getContent()
+                  if (content) {
+                      const b = content.querySelector('b')
+                      if (b) {
+                      b.textContent = Swal.getTimerLeft()
+                      }
+                  }
+                  }, 100)
+              },
+              onClose: () => {
+                  clearInterval(timerInterval)
+              }
+              }).then((result) => {
+              /* Read more about handling dismissals below */
+              if (result.dismiss === Swal.DismissReason.timer) {
+                  location.reload();
+              }
+})
+          })
+         
+      }
+  })
 }
 
-function listar_usuario() {
+function listar_usuario(){
   var table = $("#tabla_usuario").DataTable({
-    ordering: false,
-    paging: false,
-    searching: { regex: true },
-    lengthMenu: [
-      [10, 25, 50, 100, -1],
-      [10, 25, 50, 100, "All"],
-    ],
-    pageLength: 10,
-    destroy: true,
-    async: false,
-    processing: true,
-    ajax: {
-      url: "../controlador/usuario/controlador_usuario_listar.php",
-      type: "POST",
-    },
-    columns: [
-      { data: "posicion" },
-      { data: "usu_nombre" },
-      { data: "rol_nombre" },
-      {
-        data: "usu_sexo",
-        render: function (data, type, row) {
-          if (data == "M") {
-            return "MASCULINO";
-          } else {
-            return "FEMENINO";
-          }
-        },
-      },
-      {
-        data: "usu_estatus",
-        render: function (data, type, row) {
-          if (data == "ACTIVO") {
-            return "<span class='label label-success'>" + data + "</span>";
-          } else {
-            return "<span class='label label-danger'>" + data + "</span>";
-          }
-        },
-      },
-      {
-        defaultContent:
-          "<button style='font-size:13px;' type='button' class='editar btn btn-primary'>",
-      },
-    ],
+     "ordering":false,
+     "paging": false,
+     "searching": { "regex": true },
+     "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ],
+     "pageLength": 10,
+     "destroy":true,
+     "async": false ,
+     "processing": true,
+     "ajax":{
+         "url":"../controlador/usuario/controlador_usuario_listar.php",
+         type:'POST'
+     },
+     "columns":[
+         {"data":"posicion"},
+         {"data":"usu_nombre"},
+         {"data":"rol_nombre"},
+         {"data":"usu_sexo",
+              render: function (data, type, row ) {
+                  if(data=='M'){
+                      return "MASCULINO";                   
+                  }else{
+                      return "FEMINO";                 
+                  }
+              }
+         }, 
+         {"data":"usu_estatus",
+           render: function (data, type, row ) {
+             if(data=='ACTIVO'){
+                 return "<span class='label label-success'>"+data+"</span>";                   
+             }else{
+               return "<span class='label label-danger'>"+data+"</span>";                 
+             }
+           }
+         },  
+         {"defaultContent":"<button style='font-size:13px;' type='button' class='editar btn btn-primary'><i class='fa fa-edit'></i></button>"}
+     ],
 
-    language: idioma_espanol,
-    select: true,
+     "language":idioma_espanol,
+     select: true
+ });
+ document.getElementById("tabla_usuario_filter").style.display="none";
+ $('input.global_filter').on( 'keyup click', function () {
+      filterGlobal();
+  } );
+  $('input.column_filter').on( 'keyup click', function () {
+      filterColumn( $(this).parents('tr').attr('data-column') );
   });
+
+}
+
+function filterGlobal() {
+  $('#tabla_usuario').DataTable().search(
+      $('#global_filter').val(),
+  ).draw();
 }
--- vista/usuario/vista_usuario_listar.php
+++ vista/usuario/vista_usuario_listar.php
@@ -4,43 +4,54 @@
         <div class="box-header with-border">
               <h3 class="box-title">BIENVENIDO AL CONTENIDO DEL USUARIO</h3>
 
-      <div class="box-tools pull-right">
-        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-        </button>
-      </div>
-      <!-- /.box-tools -->
+            <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+            </div>
+              <!-- /.box-tools -->
+        </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+            <div class="form-group">
+                <div class="col-lg-10">
+                    <div class="input-group">
+                        <input type="text" class="global_filter form-control" id="global_filter" placeholder="Ingresar dato a buscar">
+                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <button class="btn btn-danger" style="width:100%"><i class="glyphicon glyphicon-plus"></i> Nuevo Registro</button>
+                </div>
+            </div>
+            <table id="tabla_usuario" class="display responsive nowrap" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Usuario</th>
+                        <th>Rol</th>
+                        <th>Sexo</th>
+                        <th>Estatus</th>
+                        <th>Acci&oacute;n</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>#</th>
+                        <th>Usuario</th>
+                        <th>Rol</th>
+                        <th>Sexo</th>
+                        <th>Estatus</th>
+                        <th>Acci&oacute;n</th>
+                    </tr>
+                </tfoot>
+            </table>
+            </div>
+            <!-- /.box-body -->
     </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-      <table id="tabla_usuario" class="display responsive nowrap" style="width:100%">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Usuario</th>
-            <th>Rol</th>
-            <th>Sexo</th>
-            <th>Estatus</th>
-            <th>Acci&oacute;n</th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <th>#</th>
-            <th>Usuario</th>
-            <th>Rol</th>
-            <th>Sexo</th>
-            <th>Estatus</th>
-            <th>Acci&oacute;n</th>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-    <!-- /.box-body -->
-  </div>
-  <!-- /.box -->
+          <!-- /.box -->
 </div>
 <script>
-  $(document).ready(function() {
-    $('#example').DataTable();
-  });
+$(document).ready(function() {
+    listar_usuario();
+} );
 </script>
