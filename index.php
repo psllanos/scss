e28b5003d8a03a6f9f762f1e668e42c2631b7652
samuel llanos <psllanos@misena.edu.co>
2021-10-11 12:08:07 -0500

    se crea un loader en la aplicacion

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?php
-    header('Location: Login/index.php')
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./preloader/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="./preloader/js/bootstrap.min.js"></script>
+    <script src="./preloader/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" href="./preloader/css/style.css">
+    <script src="./preloader/js/script.js"></script>
+    <meta http-equiv="refresh" content="6.9;url=./Login/index.php">
+</head>
 
+<body>
+    <div class="wrapper">
+        <div class="up"></div>
+        <div class="loading"></div>
+        <div class="down"></div>
+    </div>
+</body>
 
-?>
+</html>
